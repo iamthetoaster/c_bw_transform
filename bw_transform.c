885522bf11f560b991d3fe5de9d95d2bb7588594
Toaster <14080501+iamthetoaster@users.noreply.github.com>
2023-06-05 13:05:33 -0700

    Initial commit

---
+++ bw_transform.c
@@ -0,0 +1,6 @@
+#include "bw_transform.h"
+
+// start text: \x02
+// end text: \x03
+
+// TODO: Write the program
---
+++ bw_transform.h
@@ -0,0 +1,7 @@
+#ifndef BW_TRANSFORM_H_
+#define BW_TRANSFORM_H_
+
+// TODO: Design the interface
+
+
+#endif // BW_TRANSFORM_H_
